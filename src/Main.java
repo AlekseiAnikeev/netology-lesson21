8322a06b6b254b6ad307544128b1716a745e22be
AlekseiAnikeev <info.anikeev@gmail.com>
2022-08-18 16:42:36 +0300

    added word limit to comparator

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,21 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Владислав", "Силантьев", 70));
-        people.add(new Person("Михаил", "Аникеев", 75));
-        people.add(new Person("Светлана", "Силантьева", 30));
-        people.add(new Person("Александра", "Аникеева", 10));
+        people.add(new Person("Владислав", "Силантьев Аникеев", 70));
+        people.add(new Person("Михаил", "Аникеев Силантьев Силантьев", 75));
+        people.add(new Person("Светлана", "Силантьева Аникеев Силантьев", 30));
+        people.add(new Person("Александра", "Аникеева Силантьев", 10));
         System.out.println(people);
-        people.sort(new PersonComparator());
+        people.sort(new PersonComparator(2));
         System.out.println(people);
         System.out.println();
         List<Person> people2 = new ArrayList<>();
-        people2.add(new Person("Светлана", "Силантьева", 30));
-        people2.add(new Person("Светлана", "Силантьева", 35));
-        people2.add(new Person("Светлана", "Силантьева", 22));
-        people2.add(new Person("Светлана", "Силантьева", 15));
+        people2.add(new Person("Светлана", "Силантьева Ибн Рахат", 30));
+        people2.add(new Person("Светлана", "Силантьева Ибн Рахат", 35));
+        people2.add(new Person("Светлана", "Силантьева Ибн Рахат", 22));
+        people2.add(new Person("Светлана", "Силантьева Ибн Рахат Лукум", 15));
         System.out.println(people2);
-        people2.sort(new PersonComparator());
+        people2.sort(new PersonComparator(3));
         System.out.println(people2);
     }
 }
--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -5,14 +5,26 @@ import java.util.Comparator;
  * Date of creation: 18.08.2022
  */
 public class PersonComparator implements Comparator<Person> {
+    private final int wordCount;
+
+    public PersonComparator(int wordCount) {
+        this.wordCount = wordCount;
+    }
+
     @Override
     public int compare(Person o1, Person o2) {
-        if (o1.getSurname().length() > o2.getSurname().length()) {
-            return 1;
-        } else if (o1.getSurname().length() < o2.getSurname().length()) {
-            return -1;
+        String[] firstNameCount = o1.getSurname().split(" ");
+        String[] secondNameCount = o2.getSurname().split(" ");
+        if (firstNameCount.length >= wordCount && secondNameCount.length >= wordCount) {
+            if (firstNameCount.length != secondNameCount.length) {
+                return Integer.compare(firstNameCount.length, secondNameCount.length);
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
         } else {
             return Integer.compare(o1.getAge(), o2.getAge());
         }
     }
 }
+
+
