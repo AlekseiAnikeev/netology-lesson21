43208470ec334d69704bc8420f310082f302fa9e
AlekseiAnikeev <info.anikeev@gmail.com>
2022-08-30 23:36:47 +0300

    add lambda branch

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,17 @@ public class Main {
         people.add(new Person("Светлана", "Силантьева Аникеев Силантьев", 30));
         people.add(new Person("Александра", "Аникеева Силантьев", 10));
         System.out.println(people);
-        people.sort(new PersonComparator(2));
+        people.sort((o1,o2) -> {String[] firstNameCount = o1.getSurname().split(" ");
+            String[] secondNameCount = o2.getSurname().split(" ");
+            if (firstNameCount.length >= 2 && secondNameCount.length >= 2) {
+                if (firstNameCount.length != secondNameCount.length) {
+                    return Integer.compare(firstNameCount.length, secondNameCount.length);
+                } else {
+                    return Integer.compare(o1.getAge(), o2.getAge());
+                }
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }});
         System.out.println(people);
         System.out.println();
         List<Person> people2 = new ArrayList<>();
@@ -22,7 +32,19 @@ public class Main {
         people2.add(new Person("Светлана", "Силантьева Ибн Рахат", 22));
         people2.add(new Person("Светлана", "Силантьева Ибн Рахат Лукум", 15));
         System.out.println(people2);
-        people2.sort(new PersonComparator(3));
+        people2.sort((o1, o2) -> {
+            String[] firstNameCount = o1.getSurname().split(" ");
+            String[] secondNameCount = o2.getSurname().split(" ");
+            if (firstNameCount.length >= 3 && secondNameCount.length >= 3) {
+                if (firstNameCount.length != secondNameCount.length) {
+                    return Integer.compare(firstNameCount.length, secondNameCount.length);
+                } else {
+                    return Integer.compare(o1.getAge(), o2.getAge());
+                }
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+        });
         System.out.println(people2);
     }
 }
