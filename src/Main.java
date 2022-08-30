48392d68ee809ada166aa36a47883803884bd496
AlekseiAnikeev <info.anikeev@gmail.com>
2022-08-30 23:58:58 +0300

    add eraser branch

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
         people.add(new Person("Михаил", "Аникеев Силантьев Силантьев", 75));
         people.add(new Person("Светлана", "Силантьева Аникеев Силантьев", 30));
         people.add(new Person("Александра", "Аникеева Силантьев", 10));
+        people.add(new Person("Александра", "Аникеева Силантьев Славроса", 14));
         System.out.println(people);
         people.sort(new PersonComparator(2));
         System.out.println(people);
@@ -21,8 +22,15 @@ public class Main {
         people2.add(new Person("Светлана", "Силантьева Ибн Рахат", 35));
         people2.add(new Person("Светлана", "Силантьева Ибн Рахат", 22));
         people2.add(new Person("Светлана", "Силантьева Ибн Рахат Лукум", 15));
+        people2.add(new Person("Александра", "Аникеева Силантьев Славроса", 14));
         System.out.println(people2);
         people2.sort(new PersonComparator(3));
+        System.out.println(people2);
+        System.out.println();
+        people.removeIf((o1) -> o1.getAge() < 18);
+        System.out.println(people);
+        System.out.println();
+        people2.removeIf((o1) -> o1.getAge() < 18);
         System.out.println(people2);
     }
 }
